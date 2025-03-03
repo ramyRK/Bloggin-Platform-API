dbc02feaf65581c27f558abf19769424f9bfecbb
ramyRK <kheliliramy0@gmail.com>
2025-03-03 14:18:48 +0100

    create post model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class post (models.Model):
+    category_choices=[
+        ('Technology','Technology'),
+        ('Sports','Sports'),
+        ('Education','Education'),
+        ('Health','Health'),
+    ]
+
+    title= models.CharField(max_length=255)
+    content=models.TextField()
+    category=models.CharField(choices=category_choices)
+    tags=models.JSONField(default=list)
